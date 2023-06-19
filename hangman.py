c02c2f7a392a989e4abe49365d4e391e980f4363
rphan625 <richardphan625@gmail.com>
2023-06-18 19:05:50 -0700

    Added feature where uppercase letters can be in list of potential words, and logic checks for upper case letters

--- hangman.py
+++ hangman.py
@@ -8,27 +8,35 @@ alreadyguessed = []
 wrongguesses = []
 wrongcount = 0
 
-words = ["snake", "book", "python", "words", "game"]
+words = ["snake", "book", "python", "words", "game", "PROGRAMMER"]
 result = random.choice(words)
+print(result)
 result_split = [*result]
+print(result_split)
+
 
 def displayguessed():
     total_correct_letters = 0
     displaystring = ""
     for i in result_split:
-        if i in alreadyguessed:
+        uppercase = i.upper()
+        # print(uppercase)
+        lowercase = i.lower()
+        # print(lowercase)
+        if uppercase in alreadyguessed or lowercase in alreadyguessed:
             displaystring += i
-            displaystring += ' '
+            displaystring += " "
             total_correct_letters += 1
         else:
-            displaystring += '_'
-            displaystring += ' '
+            displaystring += "_"
+            displaystring += " "
     return displaystring, total_correct_letters
 
+
 def displayhangman(failcount):
-    match failcount:
-        case 1:
-            print('''
+    if failcount == 1:
+        print(
+            """
             =========================================
             |       __        ___          __       |
             | |__| |__| |\ | | __  |\  /| |__| |\ | |
@@ -40,12 +48,14 @@ def displayhangman(failcount):
             |       |                               |
             |       |                               |
             |       _________________               |
-            ''')
-            print(f"            |               {displayguessed()[0]}")
-            print("             =========================================\n")
-            
-        case 2:
-            print('''
+            """
+        )
+        print(f"            |               {displayguessed()[0]}")
+        print("             =========================================\n")
+
+    elif failcount == 2:
+        print(
+            """
             =========================================
             |       __        ___          __       |
             | |__| |__| |\ | | __  |\  /| |__| |\ | |
@@ -57,11 +67,13 @@ def displayhangman(failcount):
             |       |                               |
             |       |                               |
             |       _________________               |
-            ''')
-            print(f"            |               {displayguessed()[0]}")
-            print("             =========================================\n")
-        case 3:
-            print('''
+            """
+        )
+        print(f"            |               {displayguessed()[0]}")
+        print("             =========================================\n")
+    elif failcount == 3:
+        print(
+            """
             =========================================
             |       __        ___          __       |
             | |__| |__| |\ | | __  |\  /| |__| |\ | |
@@ -73,11 +85,13 @@ def displayhangman(failcount):
             |       |                               |
             |       |                               |
             |       _________________               |
-            ''')
-            print(f"            |               {displayguessed()[0]}")
-            print("             =========================================\n")
-        case 4:
-            print('''
+            """
+        )
+        print(f"            |               {displayguessed()[0]}")
+        print("             =========================================\n")
+    elif failcount == 4:
+        print(
+            """
             =========================================
             |       __        ___          __       |
             | |__| |__| |\ | | __  |\  /| |__| |\ | |
@@ -89,11 +103,13 @@ def displayhangman(failcount):
             |       |                               |
             |       |                               |
             |       _________________               |
-            ''')
-            print(f"            |               {displayguessed()[0]}")
-            print("             =========================================\n")
-        case 5:
-            print('''
+            """
+        )
+        print(f"            |               {displayguessed()[0]}")
+        print("             =========================================\n")
+    elif failcount == 5:
+        print(
+            """
             =========================================
             |       __        ___          __       |
             | |__| |__| |\ | | __  |\  /| |__| |\ | |
@@ -105,11 +121,13 @@ def displayhangman(failcount):
             |       |          /                    |
             |       |                               |
             |       _________________               |
-            ''')
-            print(f"            |               {displayguessed()[0]}")
-            print("             =========================================\n")
-        case 6:
-            print('''
+            """
+        )
+        print(f"            |               {displayguessed()[0]}")
+        print("             =========================================\n")
+    elif failcount == 6:
+        print(
+            """
             =========================================
             |       __        ___          __       |
             | |__| |__| |\ | | __  |\  /| |__| |\ | |
@@ -121,11 +139,13 @@ def displayhangman(failcount):
             |       |          / \                  |
             |       |                               |
             |       _________________               |
-            ''')
-            print(f"            |               {displayguessed()[0]}")
-            print("             =========================================\n")
-        case default:
-            print('''
+            """
+        )
+        print(f"            |               {displayguessed()[0]}")
+        print("             =========================================\n")
+    else:
+        print(
+            """
             =========================================
             |       __        ___          __       |
             | |__| |__| |\ | | __  |\  /| |__| |\ | |
@@ -137,25 +157,25 @@ def displayhangman(failcount):
             |       |                               |
             |       |                               |
             |       _________________               |
-            ''')
-            print(f"            |               {displayguessed()[0]}")
-            print("             =========================================\n")
-        
+            """
+        )
+        print(f"            |               {displayguessed()[0]}")
+        print("             =========================================\n")
+
 
 while gamerun:
-    
     print(f"        Wrong letters: {(''.join(wrongguesses))}")
     displayhangman(wrongcount)
-    
+
     guess = input("Guess a letter\n")
-    
+
     if len(guess) > 1:
         print("You can only guess one letter at a time!")
     else:
         guess = guess.lower()
-        if guess in alreadyguessed:
+        if guess.lower() in alreadyguessed or guess.upper() in alreadyguessed:
             print("You already guessed that letter!")
-        elif guess in result_split:
+        elif guess.lower() in result_split or guess.upper() in result_split:
             print(f"{guess} is in the word!\n")
             alreadyguessed.append(guess)
             # print(displayguessed()[1])
@@ -171,7 +191,7 @@ while gamerun:
             if wrongcount == 6:
                 endgame = 1
                 gamerun = False
-    
+
 print(f"        Wrong letters: {(''.join(wrongguesses))}")
 displayhangman(wrongcount)
 if endgame == 0:
